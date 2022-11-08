a5a15c284ceccb74ab5cb84c2c8ed131789263d8
Felix Merk <felixmerk@fb.com>
2022-11-07 20:03:36 -0800

    [board] from_fen and pretty print

--- board.rs
+++ board.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::convert::TryFrom;
 
 pub struct ChessBoard {
     board: [[u8; 8]; 8],
@@ -15,13 +16,98 @@ impl fmt::Display for ChessBoard {
         for r in 0..8 {
             for c in 0..8 {
                 let piece = self.board[r][c];
-                match piece{
-                    0=>write!(f, "_").unwrap(),
-                    _=>write!(f, "x").unwrap(),
-                };
+                write!(f, "{}", piece_to_char(piece)).unwrap();
             }
             write!(f, "\n").unwrap();
         }
         Ok(())
     }
 }
+
+impl ChessBoard {
+    pub fn from_fen(&mut self, fen : &str)  {
+        self.board = [[0u8; 8]; 8];
+        let mut row = 0;
+        let mut col = 0;
+        for c in fen.chars() {
+            if c != ' ' {
+                let color : u8 = if c.is_lowercase() {
+                    BLACK
+                } else {
+                    WHITE
+                };
+                match c.to_ascii_lowercase() {
+                    '/'=> {
+                        col = 0;
+                        row += 1;
+                    }
+                    'p'=> {
+                        self.board[row][col] = PAWN | color;
+                        col += 1;
+                    }
+                    'r'=> {
+                        self.board[row][col] = ROOK | color;
+                        col += 1;
+                    }
+                    'n'=> {
+                        self.board[row][col] = KNIGHT | color;
+                        col += 1;
+                    }
+                    'b'=> {
+                        self.board[row][col] = BISHOP | color;
+                        col += 1;
+                    }
+                    'k'=> {
+                        self.board[row][col] = KING | color;
+                        col += 1;
+                    }
+                    'q'=> {
+                        self.board[row][col] = QUEEN | color;
+                        col += 1;
+                    }
+                    '1'..='8'=> {
+                        col += usize::try_from(c.to_digit(10).unwrap()).unwrap();
+                    }
+                    _ => println!("Unsupported fen char"),
+                };
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+
+const WHITE : u8 = 0b1000;
+const BLACK : u8 = 0b0000;
+const KING : u8 = 0b0001;
+const QUEEN : u8 = 0b0010;
+const PAWN : u8 = 0b0011;
+const ROOK : u8 = 0b0100;
+const KNIGHT : u8 = 0b0101;
+const BISHOP : u8 = 0b0111;
+
+pub fn piece_to_char(piece : u8) -> char {
+    let piece_char : char = match clear_piece_color(piece) {
+        KING=>'k',
+        QUEEN=>'q',
+        PAWN=>'p',
+        ROOK=>'r',
+        KNIGHT=>'n',
+        BISHOP=>'b',
+        0=>'_',
+        6_u8 | 8_u8..=u8::MAX => 'X',
+    };
+
+    if (piece & WHITE) == WHITE {
+        piece_char.to_ascii_uppercase()
+    } else {
+        piece_char
+    }
+}
+
+pub fn clear_piece_color(piece : u8) -> u8 {
+    piece & !WHITE
+}
+
+
--- repl.rs
+++ repl.rs
@@ -8,6 +8,17 @@ fn main() {
 
     let mut stuff2 = [[0u8; 8]; 8];
     stuff2 [0][0] = 1;
+    stuff2 [0][4] = 4;
+    stuff2 [5][4] = 9;
     let board2 = board::build_board(stuff2);
     println!("{}", board2);
+
+
+    let starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    let mut board3 = board::build_board(stuff);
+    board3.from_fen(starting_position);
+    println!("{}", board3);
+    let other_pos = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+    board3.from_fen(other_pos);
+    println!("{}", board3);
 }
